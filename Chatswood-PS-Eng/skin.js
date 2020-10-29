200984b925a54e5a4aaea8b127f690ccd1b21f48
PippaLawrie <66765770+PippaLawrie@users.noreply.github.com>
2020-10-29 16:37:07 +1030

    Thumbnails update

--- Chatswood-PS-Eng/skin.js
+++ Chatswood-PS-Eng/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.9/17985
 // Filename: ChatswoodPublic.ggsk
-// Generated 2020-10-29T12:48:51
+// Generated 2020-10-29T16:35:58
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
