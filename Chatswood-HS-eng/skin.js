f68443a42efec0270a51b342a8519c394f6c4683
PippaLawrie <66765770+PippaLawrie@users.noreply.github.com>
2020-11-11 09:26:00 +1030

    Char leg fix

--- Chatswood-HS-eng/skin.js
+++ Chatswood-HS-eng/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.9/17985
 // Filename: ChatswoodHS-Redesign.ggsk
-// Generated 2020-11-11T09:22:44
+// Generated 2020-11-11T09:25:43
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, true);
