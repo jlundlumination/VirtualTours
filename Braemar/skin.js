54b9192de6cf149a9f6e61519286c95e6d2f094b
PippaLawrie <66765770+PippaLawrie@users.noreply.github.com>
2021-05-20 11:48:51 +0930

    Buses - smaller file

--- Braemar/skin.js
+++ Braemar/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.11/18043
 // Filename: Braemar.ggsk
-// Generated 2021-05-20T11:24:11
+// Generated 2021-05-20T11:47:49
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, false);
