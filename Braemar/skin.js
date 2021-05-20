8add96c1c0cf848ee60fc70f49aaaaec20c2f89c
PippaLawrie <66765770+PippaLawrie@users.noreply.github.com>
2021-05-20 14:38:46 +0930

    Extra photoshopping

--- Braemar/skin.js
+++ Braemar/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.11/18043
 // Filename: Braemar.ggsk
-// Generated 2021-05-20T13:58:58
+// Generated 2021-05-20T14:38:23
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, false);
