377db5cc3b03453bcdc96fe9c4a368903288a836
PippaLawrie <66765770+PippaLawrie@users.noreply.github.com>
2021-06-17 10:23:01 +0930

    Ran the new photos through Lightroom Presets

--- Bundarra-c/skin.js
+++ Bundarra-c/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.11/18043
 // Filename: Bundarra.ggsk
-// Generated 2021-06-17T09:59:35
+// Generated 2021-06-17T10:20:31
 
 function pano2vrSkin(player,base) {
 	player.addVariable('opt_hotspot_preview', 2, false);
